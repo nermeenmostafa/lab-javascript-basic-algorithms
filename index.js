c005f857e29c314b723160e77e01762db6351290
Nermeen Mostafa <nmastafa27@gmail.com>
2023-07-24 11:17:33 +0200

    done

--- index.js
+++ index.js
@@ -1,7 +1,104 @@
+console.log("Iam ready!");
+
 // Iteration 1: Names and Input
+
+let hacker1= "mahmoud";
+
+console.log(`The driver's name is ${hacker1}` );
+
+let hacker2= "salem";
+
+console.log(`The navigator's name is ${hacker2}`);
 
 
 // Iteration 2: Conditionals
 
+if(hacker1.length > hacker2.length){
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters`);
+}
+if else(hacker1.length > hacker2.length){
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters`)
+}
+else {
+    console.log(`Wow, you both have equally long names, ${hacker1.length} characters`)
+}
 
 // Iteration 3: Loops
+
+let driversName = "";
+for(let i=0; i < hacker1.length; i++) {
+    newDriversName += hacker1[i].toUpperCase() + " ";
+  }
+  console.log(newDriversName.trim());
+
+ 
+// 3.2 Print all the characters of the navigator's name, in reverse order. i.e. "nhoJ"
+let newNavigatorsName = "";
+for(let i = hacker2.length - 1; i >= 0; i--) {
+  newNavigatorsName += hacker2[i];
+}
+console.log(newNavigatorsName);
+
+
+/*
+3.3 Depending on the lexicographic order of the strings, print:
+- The driver's name goes first.
+- Yo, the navigator goes first definitely.
+- What?! You both have the same name?
+*/
+hacker1 = hacker1.toLowerCase();
+hacker2 = hacker2.toLowerCase();
+
+if (hacker1 > hacker2) {
+  console.log("The driver's name goes first.")
+}
+else if (hacker1 < hacker2) {
+  console.log("Yo, the navigator goes first definitely.")
+}
+else {
+  console.log("What?! You both have the same name?")
+} 
+/*
+Bonus 1:
+Go to lorem ipsum generator and:
+- Generate 3 paragraphs. Store the text in a new string variable named `longText`.
+  - Make your program count the number of words in the string.
+*/
+const paragraph = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fermentum rutrum nibh, ac efficitur augue consequat eu. Curabitur et luctus libero. Phasellus sit amet facilisis arcu. Sed auctor orci libero, in ultricies justo consequat vitae. Suspendisse vel lectus sit amet lacus aliquet laoreet. Vivamus justo elit, convallis quis accumsan facilisis, gravida vitae tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras sed scelerisque ligula. Duis id ex sed diam convallis porttitor id sit amet magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis ut massa aliquet, auctor sapien nec, semper eros.
+
+Etiam condimentum rutrum arcu a sagittis. Quisque auctor mi sit amet nunc ornare fermentum. Mauris consequat ultrices volutpat. Donec nec finibus eros. Sed vel accumsan nulla. Nullam vitae elit commodo, mattis arcu id, tincidunt mi. Sed volutpat nulla a bibendum pretium.
+
+Suspendisse eleifend, quam in lobortis vestibulum, odio augue facilisis lectus, ut semper elit neque ac sapien. Maecenas a est placerat augue consectetur tincidunt. Praesent auctor, tortor non varius ultrices, risus magna gravida nulla, vitae suscipit purus turpis et elit. Nam mauris est, hendrerit vitae euismod sit amet, egestas sit amet justo. Vivamus dapibus a leo ac sollicitudin. Donec fringilla ac arcu eu molestie. Aliquam bibendum venenatis gravida. Integer dapibus placerat sapien vulputate auctor. Nulla tincidunt vestibulum elit. Ut at malesuada leo, sit amet fermentum est. Duis sit amet libero quis mi fringilla condimentum. Maecenas fringilla tellus ac enim finibus viverra. Proin a posuere ex.`
+
+let words = 0;
+let etCount = 0;
+
+let splittedParagraph = paragraph.split(' ');
+
+for (let i=0; i < splittedParagraph.length; i++) {
+  if(splittedParagraph[i].includes('\n')) {
+    words++
+  } else if(splittedParagraph[i] === "et" || splittedParagraph[i] === "et.") {
+    etCount++;
+  }
+  words++;
+ }
+
+console.log('words -->', words);
+console.log('et count  -->', etCount);
+/*
+Bonus 2:
+Create a new variable, `phraseToCheck`, containing some string value. Write a code to check
+ if the value assigned to this variable is a [Palindrome](https://en.wikipedia.org/wiki/Palindrome).
+*/
+let stringToCheck = "A man, a plan, a canal, Panama!";
+stringToCheck = stringToCheck.replace(/[ ,!.]/g, "").toLowerCase();
+
+let reversedString = stringToCheck.split('').reverse().join('');
+
+if (stringToCheck === reversedString) {
+  console.log('String is a palindrome');
+}
+else {
+  console.log('Not a palindrome');
+}
